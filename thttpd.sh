325908459b5b78f20de528c0e62f4a7017cd9d15
Michael Reiley <mreiley@pariahsoft.com>
2015-08-31 20:16:26 -0700

    ...

--- thttpd.sh
+++ thttpd.sh
@@ -5,12 +5,12 @@
 INDEX="index.cgi"
 PORT="8182"
 DIR=`pwd`
-WILD="**index.cgi"
 USER="nobody"
+WILD="**index.cgi"
 HOST="localhost"
 LOG=`pwd`/"pyww.log"
 PID=`pwd`/"pyww.pid"
 CSET="UTF-8"
 
-thttpd -p $PORT -d $DIR -dd $DIR -c $WILD -h $HOST -l $LOG -i $PID -T $CSET $*
+thttpd -p $PORT -d $DIR -dd $DIR -u $USER -c $WILD -h $HOST -l $LOG -i $PID -T $CSET $*
 
